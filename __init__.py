2068a44a7784ef191b613a993b2a148f99b53b25
al-ov73 <klever1987@yandex.ru>
2023-11-15 20:29:33 +0200

    init app

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from page_analyzer import app
---
+++ page_analyzer/app.py
@@ -0,0 +1,19 @@
+from flask import (
+Flask, redirect,
+render_template,
+request,
+flash,
+url_for,
+get_flashed_messages
+)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+if __name__ == "__main__":
+    index()
