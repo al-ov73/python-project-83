fa04d4d131da93bd49c8c523aa7b2c2b3ddc439e
al-ov71 <klever1987@yandex.ru>
2023-11-17 01:00:48 +0400

    conect db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,9 +6,11 @@ flash,
 url_for,
 get_flashed_messages
 )
-
+import validators
 import psycopg2
 import os
+from datetime import date, datetime
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -21,19 +23,34 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/urls', methods=['POST'])
-def create():
-    # Get the username and email from the request body
-    username = request.form.get('username')
-    email = request.form.get('email')
+
+@app.post('/urls')
+def get_url():
+    received_url = request.form.get('url')
+    if validators.url(received_url):
+        parsed_url = urlparse(received_url).hostname
  
-    # Insert the data into the database
-    cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO users (username, email) VALUES (%s, %s)", (username, email))
-    conn.commit()
+        created = datetime.now()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (parsed_url, created))
+        conn.commit()
+    else:
+        print('not valid')
+    return redirect(url_for('index'))
+
+# @app.route('/urls', methods=['POST'])
+# def create():
+#     # Get the username and email from the request body
+#     username = request.form.get('username')
+#     email = request.form.get('email')
  
-    return 'User created successfully!'
+#     # Insert the data into the database
+#     cur = conn.cursor()
+#     cur.execute(
+#         "INSERT INTO users (username, email) VALUES (%s, %s)", (username, email))
+#     conn.commit()
+ 
+#     return 'User created successfully!'
  
  
 if __name__ == '__main__':
