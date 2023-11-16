1ac3fbcaaf716a4bb28b7ba79644a4c214002fb2
al-ov73 <143029512+al-ov73@users.noreply.github.com>
2023-11-16 13:50:14 +0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,47 +1,8 @@
-DROP TABLE IF EXISTS topics;
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+DROP TABLE IF EXISTS urls;
+
+CREATE TABLE urls (
   id bigint PRIMARY KEY,
-  username VARCHAR(50) UNIQUE NOT NULL,
-  birthday DATE,
-  email_confirmed BOOLEAN,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  gender VARCHAR(255) NOT NULL,
-  password_digest VARCHAR(255) NOT NULL,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
+  name VARCHAR(255) UNIQUE NOT NULL,
   created_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE topics (
-  id bigint PRIMARY KEY,
-  user_id bigint REFERENCES users(id) NOT NULL,
-  title varchar(255),
-  body text,
-  created_at TIMESTAMP NOT NULL
-);
-
-CREATE TABLE courses (
-  id bigint PRIMARY KEY,
-  name varchar(255),
-  body text,
-  created_at timestamp
-);
-
-CREATE TABLE course_members (
-  id bigint PRIMARY KEY,
-  user_id bigint REFERENCES users(id) NOT NULL,
-  course_id bigint REFERENCES courses(id) NOT NULL,
-  created_at timestamp
-);
-
-CREATE TABLE course_reviews (
-  id bigint PRIMARY KEY,
-  course_member_id bigint REFERENCES course_members(id) NOT NULL,
-  course_id bigint REFERENCES courses(id) NOT NULL,
-  user_id bigint REFERENCES users(id) NOT NULL,
-  spent_minutes int NOT NULL,
-  rating int,
-  body text,
-  created_at timestamp
-);
