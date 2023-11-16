77d169c2ef7c274091fa65f12bacfa74ea2a3bc9
al-ov73 <143029512+al-ov73@users.noreply.github.com>
2023-11-16 13:36:17 +0400

    Update app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,6 +7,12 @@ url_for,
 get_flashed_messages
 )
 
+import psycopg2
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)
 
