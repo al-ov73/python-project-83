9b5c5b8e3b5bffef15fc9a9acd56f53786a9aeca
al-ov71 <klever1987@yandex.ru>
2023-11-17 23:58:45 +0400

    add page-analyzer and url list

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -12,16 +12,23 @@ import os
 from datetime import date, datetime
 from urllib.parse import urlparse
 from dotenv import load_dotenv
+
+
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)
-
+app.secret_key = "secret_key"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    messages = get_flashed_messages(with_categories=True)
+    print(messages)
+    return render_template(
+        'index.html',
+        messages=messages,
+        )
 
 
 @app.post('/urls')
@@ -29,29 +36,56 @@ def get_url():
     received_url = request.form.get('url')
     if validators.url(received_url):
         parsed_url = urlparse(received_url).hostname
- 
         created = datetime.now()
-        cur = conn.cursor()
-        cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (parsed_url, created))
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM urls")
+        in_db = cursor.fetchall()
+
+        if parsed_url in [url[0] for url in in_db]:
+            flash('Url already in list.', 'warning')
+        else:
+            flash('Url was added to list.', 'success')
+            cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (parsed_url, created))
+        cursor.execute("SELECT * FROM urls WHERE name = %s", (parsed_url,))
+        received_url = cursor.fetchall()
+        id = received_url[0][0]
+        name = received_url[0][1]
         conn.commit()
     else:
-        print('not valid')
-    return redirect(url_for('index'))
+        flash('Url is not valid!', 'warning')
+        return redirect(url_for('index'))   
+    return redirect(
+        url_for('url_info', id=id),
+    )
 
-# @app.route('/urls', methods=['POST'])
-# def create():
-#     # Get the username and email from the request body
-#     username = request.form.get('username')
-#     email = request.form.get('email')
+@app.get('/urls/<id>')
+def url_info(id):
+    messages = get_flashed_messages(with_categories=True)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM urls WHERE id = %s", (id,))
+    received_url = cursor.fetchall()
+    name = received_url[0][1]
+    created = received_url[0][2]
+    return render_template(
+        'info.html',
+        id=id,
+        name=name,
+        created=created,
+        messages=messages,
+    )
+
+@app.get('/urls')
+def create():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM urls")
+    urls_list = cursor.fetchall()
+    conn.commit()
  
-#     # Insert the data into the database
-#     cur = conn.cursor()
-#     cur.execute(
-#         "INSERT INTO users (username, email) VALUES (%s, %s)", (username, email))
-#     conn.commit()
+    return render_template(
+        'urls.html',
+        urls_list=urls_list,
+    )
  
-#     return 'User created successfully!'
- 
- 
+
 if __name__ == '__main__':
     app.run()
