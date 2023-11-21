2252f2d79c7d5dda95b1b68f57558f93456b2f90
al-ov71 <klever1987@yandex.ru>
2023-11-21 23:38:38 +0400

    rm raise_for_status(

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -28,7 +28,7 @@ app.secret_key = os.getenv('SECRET_KEY')
 def response_from(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()
+        # response.raise_for_status()
         return response
     except requests.exceptions.RequestException:
         flash('Произошла ошибка при проверке', 'danger')
